8cd15c80d4c7e7c26cc6392f043023fbfa5959bc
Leah Torregiano <lct2986@rit.edu>
2024-11-16 00:49:28 -0500

    Add input manager

--- Scare Parts/Assets/Scripts/BulletManager.cs
+++ Scare Parts/Assets/Scripts/BulletManager.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BulletManager : MonoBehaviour
 {
+    // fields
+    [SerializeField] private GameObject player;
+    public List<GameObject> enemyBullets;
+    public List<GameObject> playerBullets;
+    [SerializeField] private GameObject bullet;
+    public float speed = 6.0f;
+    private InputActionReference fireAction;
+    [SerializeField] private Camera cam;
+    private float camWidth;
+
+
     // Start is called before the first frame update
     void Start()
     {
