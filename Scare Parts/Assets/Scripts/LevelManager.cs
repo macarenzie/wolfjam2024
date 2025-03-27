bbb5106c1367ce57ac2ab547a7b511703d88885b
arielnotoreo <arielcthwe@gmail.com>
2025-03-27 12:06:16 -0400

    Revert "Fixed BGM"
    This reverts commit 3ce64fd8521f93133c37a4ed88169f71134cb74b.

--- Scare Parts/Assets/Scripts/LevelManager.cs
+++ Scare Parts/Assets/Scripts/LevelManager.cs
@@ -20,10 +20,11 @@ public class LevelManager : MonoBehaviour
     }
     private float speedScaleFactor = 1;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        AudioManager.instance.PlayMusic("SHMUP_BGM");
+        
     }
 
     // Update is called once per frame
@@ -32,13 +33,13 @@ public class LevelManager : MonoBehaviour
         // if health is 0, end game
 
         // if boost
-        if (player.BoostCurrent >= 100)
+        if(player.BoostCurrent >= 100)
         {
             player.BoostIncrease = false;
             speedScaleFactor = 2;
             AudioManager.instance.PlaySFX("TurboBoost_Activate");
         }
-        else if (player.BoostCurrent <= 0)
+        else if(player.BoostCurrent <= 0)
         {
             player.BoostIncrease = true;
             speedScaleFactor = 1;
